c932f8b6ada023dcf7be5eab9ca47f6743cec92d
Kevin Rodas <32557018+rkevinrodas@users.noreply.github.com>
2026-07-19 18:36:04 -0700

    feat(): Challenge 6: Add more information at the top of the page about the company

--- index.js
+++ index.js
@@ -147,12 +147,17 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedCount = GAMES_JSON.filter( (game) => { return game.pledged < game.goal; } ).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = 'A total of $' + totalRaised.toLocaleString() + ' has been raised for ' + 
+GAMES_JSON.length + ' games. Currently, ' + unfundedCount + ' ' + 
+(unfundedCount === 1 ? 'game remains' : 'games remain') + ' unfunded. We need your help to fund these amazing games!';
 
 // create a new DOM element containing the template string and append it to the description container
+const descriptionElement = document.createElement("p");
+descriptionElement.innerHTML = displayStr;
+descriptionContainer.appendChild(descriptionElement);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
